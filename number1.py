7c53428788c3391cbc2b807fec62b2b08ed125af
Arseniy <ambelyakov@gmail.com>
2021-09-09 19:52:12 +0300

    Небольшие улучшения

--- number1.py
+++ number1.py
@@ -5,6 +5,6 @@ def create_massive(minimum, maximum, lenght):
     return list(randint(minimum, maximum) for _ in range(lenght))
 
 
-massive = create_massive(int(input("Введите минимальное число ")), int(input("Введите максимальное число ")), int(input("Введите длинну массива ")))
+massive = create_massive(int(input("Введите минимальное число ")), int(input("Введите максимальное число ")), int(input("Введите длину массива ")))
 massive.sort()
 print(massive[-1]-massive[0])
--- number2.py
+++ number2.py
@@ -5,5 +5,5 @@ def create_massive(minimum, maximum, lenght):
     return list(randint(minimum, maximum) for _ in range(lenght))
 
 
-massive = create_massive(int(input("Введите минимальное число ")), int(input("Введите максимальное число ")), int(input("Введите длинну массива ")))
+massive = create_massive(int(input("Введите минимальное число ")), int(input("Введите максимальное число ")), int(input("Введите длину массива ")))
 print(massive)
